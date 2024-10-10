33a096f4d3aa0ea287c8dff5675c29cdee6e1534
Hernandez-Luis <luisalbertohdzrmz2003@gmail.com>
2024-10-10 11:00:40 -0600

    Actualización en submódulo 01-primeros-pasos

---
+++ src/MiComponente.js
@@ -0,0 +1,32 @@
+// Importar moduos de react
+import React from "react";
+
+
+// La funcion
+const MiComponente = () =>{
+
+    let nombre = "Wicho"
+    let web = "Wicho.web"
+    
+    let usuario = {
+        nombre: " WichoObject",
+        apellidos: "ApellidosWicho",
+        web: "Wicho.web"
+    };
+
+    return (
+        <div className="mi-componente">
+            <hr/>
+            <p>Este es mi primer componente</p>
+            <h3>Datps de l usuario:</h3>
+            <ul>
+                <li>Nombre: <strong>{usuario.nombre}</strong></li>
+                <li>Web: <strong>{usuario.web}</strong></li>
+            </ul>
+            <p>Este es el segundo parrafo de mi componente</p>
+        </div>
+    ); 
+};
+
+//Export
+export default MiComponente;
