603041401f546d7316216f4bf4298a869b1c73ad
Hernandez-Luis <luisalbertohdzrmz2003@gmail.com>
2024-10-23 15:57:55 -0600

    Se recuperaron los usuarios por medio de ajax, se inicia con video 53

--- 03-use-effect/src/App.js
+++ 03-use-effect/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { PruebasComponents } from './components/PruebasComponents';
+import { AjaxComponent } from './components/AjaxComponent';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <PruebasComponents></PruebasComponents>
+        <hr></hr>
+        <AjaxComponent></AjaxComponent>
       </header>
     </div>
   );
--- 03-use-effect/src/components/AjaxComponent.js
+++ 03-use-effect/src/components/AjaxComponent.js
@@ -1,7 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const AjaxComponent = () => {
+
+  const [usuarios,setUsuarios] = useState([]);
+  //Generico / basico
+  const getUsuariosEstaticos = () =>{
+    setUsuarios([
+      {
+        "id":7,
+        "email":"luis@reqres.in",
+        "first_name":"Luis",
+        "last_name":"Hernandez",
+        "avatar":"https://reqres.in/img/faces/7-image.jpg"
+      },
+      {
+        "id":8,
+        "email":"Alberto@reqres.in",
+        "first_name":"Alberto",
+        "last_name":"Ramirez",
+        "avatar":"https://reqres.in/img/faces/8-image.jpg"
+      },
+      {
+        "id":9,
+        "email":"Luis@reqres.in",
+        "first_name":"Luis",
+        "last_name":"Perez",
+        "avatar":"https://reqres.in/img/faces/9-image.jpg"
+      }
+    ])
+  }
+
+  const getUsuariosAjaxPms = () =>{
+    fetch("https://reqres.in/api/users?page=1")
+      .then(respuesta => respuesta.json())
+      .then(
+        resultado_final => {
+          setUsuarios(resultado_final.data)
+          console.log(usuarios)
+      },
+      error =>{
+        console.log(error);
+      }
+    )
+  }
+
+  const getUsuariosAjaxAW = async () => {
+    const peticion = await fetch("https://reqres.in/api/users?page=1")
+    const {data} = await peticion.json();
+
+    setUsuarios(data);
+
+    console.log(data)
+  } 
+
+  useEffect(() => {
+    //getUsuariosEstaticos();
+    //getUsuariosAjaxPms();
+    getUsuariosAjaxAW();
+  },[]);
+
   return (
-    <div>AjaxComponent</div>
+    <div>
+      <h1>Listado de usuario via Ajax</h1>  
+      <ol className='usuarios'>  
+        {
+          usuarios.map(usuario =>{
+            console.log(usuario)
+            return <li key={usuario.id}>{usuario.first_name} {usuario.last_name}</li>
+          })
+        }
+      </ol>
+    </div>
   )
 }
