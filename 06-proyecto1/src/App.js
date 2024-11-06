2de8a2eec23b4df7aa9a5e04e80b3fb1f5a7bd70
Hernandez-Luis <luisalbertohdzrmz2003@gmail.com>
2024-11-06 00:25:18 -0600

    Se modificaron los componentes del index

--- 06-proyecto1/src/App.js
+++ 06-proyecto1/src/App.js
@@ -1,13 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Buscador } from "./components/Buscador";
+import { Crear } from "./components/Crear";
+import { Listados } from "./components/Listados";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-  
-      </header>
+    <div className="layout">
+        <header className="header">
+            <div className="logo">
+                <div className="play"></div>
+            </div>
+            
+            <h1>MisPelis</h1>
+        </header>
+
+        {/*Barra de navegación*/}
+        <nav className="nav">
+            <ul>
+                <li><a href="/#">Inicio</a></li>
+                <li><a href="/#">Peliculas</a></li>
+                <li><a href="/#">Blog</a></li>
+                <li><a href="/#">Contacto</a></li>
+            </ul>
+        </nav>
+
+        {/*Contenido principal*/}
+        <section id="content" className="content">
+            {/*aqui va el listado de peliculas*/}
+          <Listados></Listados>
+        </section>
+
+        {/*Barra lateral*/}
+        <aside className="lateral">
+            <Buscador></Buscador>
+            <Crear></Crear>
+        </aside>
+
+        {/*Pie de página*/}
+        <footer className="footer">
+            &copy; Máster en React - <a href="https://victorroblesweb.es">victorroblesweb.es</a>
+        </footer>
+
     </div>
   );
 }
